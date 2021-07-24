79094fd542475fdf842bcaae6a0151cde47189b6
Sreelekshmy S <sreelu28186@gmail.com>
2021-07-24 21:16:29 +0530

    updated routing module.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,6 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  // {
-  //   path: '',
-  //   loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
-  // },
   {
     path: '',
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
@@ -18,7 +14,11 @@ const routes: Routes = [
     path: 'donation-form',
     loadChildren: () => import('./donation-form/donation-form.module').then( m => m.DonationFormPageModule)
   },
-
+  {
+    path: '',
+    redirectTo: 'donor',
+    pathMatch: 'full'
+  }
 ];
 @NgModule({
   imports: [
