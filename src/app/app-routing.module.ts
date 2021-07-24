52bec88d0bc9082324ef620d349cdfa76046aca4
Sreelekshmy S <sreelu28186@gmail.com>
2021-07-24 21:00:24 +0530

    Added & Styled home page and buttons.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,13 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
+  // {
+  //   path: '',
+  //   loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+  // },
   {
     path: '',
-    loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
+    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
   {
     path: 'donor',
@@ -13,7 +17,8 @@ const routes: Routes = [
   {
     path: 'donation-form',
     loadChildren: () => import('./donation-form/donation-form.module').then( m => m.DonationFormPageModule)
-  }
+  },
+
 ];
 @NgModule({
   imports: [
